88e7ab744b16d52135fa17aa26d4691edc5e5c2b
rayhan926 <rayhanbd7075@gmail.com>
2022-03-20 12:21:18 +0600

    Fixed link

--- components/SocialSection.js
+++ components/SocialSection.js
@@ -8,22 +8,22 @@ function SocialSection() {
             <div className="section_title">Join Our Community</div>
             <ul className="flex items-center justify-center gap-12">
                 <li>
-                    <a href="https://twitter.com/MutantApeSolana" className="text-white hover:text-secondary">
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/MutantApeSolana" className="text-white hover:text-secondary">
                         <BsTwitter size={35} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://discord.gg/VbkDnb5F" className="text-white hover:text-secondary">
+                    <a target="_blank" rel="noreferrer" href="https://discord.gg/VbkDnb5F" className="text-white hover:text-secondary">
                         <SiDiscord size={35} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/mutantapesolanaclub/" className="text-white hover:text-secondary">
+                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/mutantapesolanaclub/" className="text-white hover:text-secondary">
                         <BsInstagram size={35} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://t.me/+CyrSEkvZs8pkZWIx" className="text-white hover:text-secondary">
+                    <a target="_blank" rel="noreferrer" href="https://t.me/+CyrSEkvZs8pkZWIx" className="text-white hover:text-secondary">
                         <FaTelegramPlane size={35} />
                     </a>
                 </li>
